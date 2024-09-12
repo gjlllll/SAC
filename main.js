814948f9784d8d238df24d59fde46a1b3e7a1802
gjlllll <gjlee8712.ai@gmail.com>
2024-09-12 13:05:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 			<div id="root" style="width: 100%; height: 100%;">
 			Hello Custom Widget !!!!
 			</div>
-		  `
+		`
 		class Main extends HTMLElement {
 			constructor () {
 			super()
@@ -17,7 +17,24 @@
 				
 			this._root = this._shadowRoot.getElementById('root')
 			}
-		}
 			
+			onCustomWidgetResize (width, height) {
+				this.render()
+			}
+			
+			onCustomWidgetAfterUpdate (changedProps) {
+			}
+			
+			onCustomWidgetDestroy () {
+			}
+			
+			render () {
+				this._root.textContent = 'Hello Custom Widget clientWidth: ${this.clientWidth}, clientHeight: ${this.clientHeight}'
+			}	
+			
+		}
+		
 		customElements.define('com-sap-sac-exercise-gjlee-main', Main)
 	})()
+	
+	
